b1aa975ec86e84390f0355e20683be2bd1698411
15035648 <15035648@myrp.edu.sg>
2017-07-20 11:42:12 +0800

    commit again

--- app/src/main/java/com/example/a15035648/demolocationdetection/MainActivity.java
+++ app/src/main/java/com/example/a15035648/demolocationdetection/MainActivity.java
@@ -11,10 +11,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLocation;
@@ -43,6 +45,16 @@ public class MainActivity extends AppCompatActivity implements
                 ||  permissionCheck_Fine  == PermissionChecker.PERMISSION_GRANTED){
             mLocation = LocationServices.FusedLocationApi.getLastLocation(
                     mGoogleApiClient);
+            LocationRequest mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest
+                    .PRIORITY_BALANCED_POWER_ACCURACY);
+            mLocationRequest.setInterval(10000);
+            mLocationRequest.setFastestInterval(5000);
+            mLocationRequest.setSmallestDisplacement(100);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+
+
         } else {
             mLocation = null;
             Toast.makeText(MainActivity.this,
@@ -78,6 +90,13 @@ public class MainActivity extends AppCompatActivity implements
         if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        //the detected location is given by the variable location in the signature
+
+        Toast.makeText(this, "Lat : " + location.getLatitude() + " Lng : " +
+                location.getLongitude(), Toast.LENGTH_SHORT).show();
     }
 
 }
