da76e2c843516cd1d83242a9eee924ca931950e6
15035648 <15035648@myrp.edu.sg>
2017-07-20 10:46:41 +0800

    commit again

--- app/src/main/java/com/example/a15035648/demolocationdetection/MainActivity.java
+++ app/src/main/java/com/example/a15035648/demolocationdetection/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.a15035648.demolocationdetection;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                ||  permissionCheck_Fine  == PermissionChecker.PERMISSION_GRANTED){
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+        } else {
+            mLocation = null;
+            Toast.makeText(MainActivity.this,
+                    "Permission not granted to retrieve location info",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        if (mLocation != null) {
+            Toast.makeText(this, "Lat : " + mLocation.getLatitude() +
+                            " Lng : " + mLocation.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Location not Detected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 }
